071d288c8f1791da02c84442b198c7c133315910
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2023-08-19 12:15:22 +0500

    abc

--- controllers/gptController.js
+++ controllers/gptController.js
@@ -12,12 +12,14 @@ module.exports.gptResponse = async (req, res, next) => {
       return res.status(400).json({ message: "No record found" });
     }
 
-    let prompt = `Please analyze the provided data for company "${targetCompanyName}" across different sheets (InsigAI, Twitter, and Carbon offsets) and identify any contradictions or inconsistencies between the information provided. You can mention any conflicting mismatched details, or information that doesn't align across these sources. If there are any discrepancies, please highlight them.\n\n${JSON.stringify(
+    let prompt = `Please analyze the provided data for company "${targetCompanyName}" across different sheets (InsigAI, Twitter, and Carbon offsets) and identify any contradictions or inconsistencies between the information provided. You can mention any conflicting mismatched details, or information that doesn't align across these sources. If there are any discrepancies, please highlight them.
+    ${JSON.stringify(
       rowsOfCompany,
       null,
       2
     )}
     `;
+
 
     let response = await chapGPT(prompt);
     if (!response) {
--- utils/gpt.js
+++ utils/gpt.js
@@ -6,13 +6,19 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const chapGPT = async (prompt) => {
-  const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: prompt }],
-  });
+  const response = await openai.createChatCompletion(
+    (model = "gpt-4"),
+    // (temperature = 0),
+    (messages = [{ role: "user", content: prompt }])
+  );
+
+  // const response = await openai.createChatCompletion({
+  //   model: "gpt-4",
+  //   messages: [{ role: "user", content: prompt }],
+  // });
+  console.log("---", response.data.choices[0].message.content);
   console.log(response["data"]["choices"][0]["message"]["content"]);
   return response["data"]["choices"][0]["message"]["content"];
 };
-
 
 module.exports = { chapGPT };
