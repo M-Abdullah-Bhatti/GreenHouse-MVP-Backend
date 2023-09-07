733f47db8220497f506436af4b29b6638a514591
ShayanJamil519 <shayanjamil500@gmail.com>
2023-09-07 23:44:58 +0500

    Login Done

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser')
+const cors = require("cors")
 require("dotenv").config()
 
-
+app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
