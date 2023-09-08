98fd13d854db8e741973b32eeb9a04a535b87e06
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2023-09-08 11:21:15 +0500

    package.json updated

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser')
-const cors = require("cors")
-require("dotenv").config()
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const gptRoute = require("./routes/gptRoute");
-const regulatorRoute  = require("./routes/regulatorRoute");
+const regulatorRoute = require("./routes/regulatorRoute");
 const connectDb = require("./config/db");
 app.use("/api/gpt", gptRoute);
 app.use("/api/regulator", regulatorRoute);
 
-
-connectDb()
+connectDb();
 
 app.listen(5000, () => {
   console.log("server listening on port ", 5000);
